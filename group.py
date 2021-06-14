ead6d47097c31dfef4306ed288129ce96fca32d8
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-06-14 03:39:33 +0300

    Записанный код разбит на методы. Выделены параметры из переменных. Создан класс group_address.

--- group.py
+++ group.py
@@ -5,3 +5,28 @@ class Group():
         self.header = header
         self.footer = footer
 
+class Group_address():
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, phone_number_home,
+                        mobile, phone_number_work, email1, email2, bday, bmonth, byear, aday, amonth, ayear, address2,
+                        phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.phone_number_home = phone_number_home
+        self.mobile = mobile
+        self.phone_number_work = phone_number_work
+        self.email1 = email1
+        self.email2 = email2
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
--- test_add_new_address.py
+++ test_add_new_address.py
@@ -3,85 +3,101 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from group import Group_address
 import unittest
 
 class TestAddNewAddress(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
     
     def test_add_new_address(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/")
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("add new").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("Ivan")
-        driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys("Peter")
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("Ivanov")
-        driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys("admin911")
-        driver.find_element_by_name("title").clear()
-        driver.find_element_by_name("title").send_keys("test")
-        driver.find_element_by_name("company").clear()
-        driver.find_element_by_name("company").send_keys("OOO \"LM\"")
-        driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys("Lenina, 1")
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys("2894523")
-        driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys("8951456789")
-        driver.find_element_by_name("work").clear()
-        driver.find_element_by_name("work").send_keys("89634567823")
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("aaa@gmail.com")
-        driver.find_element_by_name("email2").clear()
-        driver.find_element_by_name("email2").send_keys("bbb@mail.ru")
-        driver.find_element_by_name("bday").click()
-        Select(driver.find_element_by_name("bday")).select_by_visible_text("1")
-        driver.find_element_by_xpath("//option[@value='1']").click()
-        driver.find_element_by_name("bmonth").click()
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("July")
-        driver.find_element_by_xpath("//option[@value='July']").click()
-        driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys("1995")
-        driver.find_element_by_name("aday").click()
-        Select(driver.find_element_by_name("aday")).select_by_visible_text("1")
-        driver.find_element_by_xpath("(//option[@value='1'])[2]").click()
-        driver.find_element_by_name("amonth").click()
-        Select(driver.find_element_by_name("amonth")).select_by_visible_text("July")
-        driver.find_element_by_xpath("(//option[@value='July'])[2]").click()
-        driver.find_element_by_name("ayear").clear()
-        driver.find_element_by_name("ayear").send_keys("2025")
-        driver.find_element_by_name("address2").clear()
-        driver.find_element_by_name("address2").send_keys("Lenina,1")
-        driver.find_element_by_name("phone2").clear()
-        driver.find_element_by_name("phone2").send_keys("Lenina,2")
-        driver.find_element_by_name("notes").clear()
-        driver.find_element_by_name("notes").send_keys("test")
-        driver.find_element_by_xpath("(//input[@value=\"Enter\"])[2]").click()
-        driver.find_element_by_link_text("home page").click()
-        driver.find_element_by_link_text("Logout").click()
-    
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.add_new_address(wd, Group_address(firstname="Ivan", middlename="Peter", lastname="Ivanov", nickname="admin911",
+                        title="test", company="OOO \"LM\"", address="Lenina, 1", phone_number_home="2894523",
+                        mobile="8951456789", phone_number_work="89634567823", email1="aaa@gmail.com", email2="bbb@mail.ru",
+                        bday="1", bmonth="July", byear="1995", aday="1", amonth="July", ayear="2025",
+                        address2="Lenina,1", phone2="Lenina,2", notes="test"))
+        self.return_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_home_page(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def add_new_address(self, wd, group):
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(group.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(group.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(group.lastname)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(group.nickname)
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(group.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(group.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(group.address)
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(group.phone_number_home)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(group.mobile)
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(group.phone_number_work)
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(group.email1)
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(group.email2)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(group.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(group.bmonth)
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(group.byear)
+        wd.find_element_by_name("aday").click()
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(group.aday)
+        wd.find_element_by_name("amonth").click()
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(group.amonth)
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(group.ayear)
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(group.address2)
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(group.phone2)
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(group.notes)
+        wd.find_element_by_xpath("(//input[@value=\"Enter\"])[2]").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
+        try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
+        try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
     
     def close_alert_and_get_its_text(self):
         try:
-            alert = self.driver.switch_to_alert()
+            alert = self.wd.switch_to_alert()
             alert_text = alert.text
             if self.accept_next_alert:
                 alert.accept()
@@ -91,7 +107,7 @@ class TestAddNewAddress(unittest.TestCase):
         finally: self.accept_next_alert = True
     
     def tearDown(self):
-        self.driver.quit()
+        self.wd.quit()
 
 if __name__ == "__main__":
     unittest.main()
