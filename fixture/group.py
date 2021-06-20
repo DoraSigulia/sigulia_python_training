e350121afb593aca6906622f0a3452754e048fd5
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-06-20 15:44:05 +0300

    Фикстура перенесена в отдельный файл. Создан тестдля удаления группы

--- fixture/group.py
+++ fixture/group.py
@@ -23,3 +23,12 @@ class GroupHelper:
     def return_to_group(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_group_page()
+        # выбрать первую группу
+        wd.find_element_by_name("selected[]").click()
+        # удалить группу
+        wd.find_element_by_name("delete").click()
+        self.return_to_group()
---
+++ test/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import pytest
+from fixture.application import Application
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
