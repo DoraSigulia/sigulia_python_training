d62b441427e8e14ef78260a697caa87e867b155c
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-06-20 14:01:42 +0300

    Исправлена ошибка в названии метода

--- fixture/group.py
+++ fixture/group.py
@@ -7,7 +7,7 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, group):
+    def create(self, group):
         wd = self.app.wd
         self.open_group_page()
         wd.find_element_by_name("new").click()
