ed6df4f2621b79ad06013f604df3945a3adfd335
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-06-26 21:55:59 +0300

    Добавлен тест на модификацию footer

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -9,3 +9,8 @@ def test_modify_group_header(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
+
+def test_modify_group_footer(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(footer="New footer"))
+    app.session.logout()
