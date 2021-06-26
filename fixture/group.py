8c7c4721c31ba1110941f185bd83437b89ffc6e7
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-06-26 21:28:35 +0300

    Создан метод модификации первой группы (без условий)

--- fixture/group.py
+++ fixture/group.py
@@ -11,26 +11,25 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         wd.find_element_by_name("new").click()
+        self.fill_group_form(group)
+        wd.find_element_by_name("submit").click()
+        self.return_to_group()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-        self.return_to_group()
 
     def edit_first(self, group):
         wd = self.app.wd
         self.open_group_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         wd.find_element_by_name("update").click()
         self.return_to_group()
 
@@ -41,8 +40,23 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_group_page()
-        # выбрать первую группу
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # удалить группу
         wd.find_element_by_name("delete").click()
+        self.return_to_group()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_group(self, new_group_date):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_first_group()
+        #открываем форму
+        wd.find_element_by_name("edit").click()
+        #заполняем форму
+        self.fill_group_form(new_group_date)
+        #сохраняем форму
+        wd.find_element_by_name("update").click()
         self.return_to_group()
