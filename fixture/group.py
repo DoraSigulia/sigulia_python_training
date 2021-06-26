3a7bf930c89f5bb003b515919a7e8569793cbdea
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-06-26 21:53:57 +0300

    Выделен метод change_field_value с проверкой заполнения поля

--- fixture/group.py
+++ fixture/group.py
@@ -17,12 +17,15 @@ class GroupHelper:
 
     def fill_group_form(self, group):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def edit_first(self, group):
         wd = self.app.wd
