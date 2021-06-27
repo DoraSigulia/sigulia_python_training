cd2666bedd17a4cae09da6f41df11dc3767e9fd6
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-06-27 15:58:01 +0300

    Добавлена проверка валидности фикстуры

--- conftest.py
+++ conftest.py
@@ -1,10 +1,22 @@
 import pytest
 from fixture.application import Application
 
-@pytest.fixture(scope = "session")
+fixture = None
+
+@pytest.fixture #(scope = "session")
 def app(request):
-    fixture = Application()
-    fixture.session.login(username="admin", password="secret")
+    global fixture
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(username="admin", password="secret")
+    return fixture
+
+@pytest.fixture(scope = "session", autouse = True)
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
