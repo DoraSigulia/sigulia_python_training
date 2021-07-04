a4b6ca2b221858676e73979c573ca2171fe67d4a
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-07-04 23:06:12 +0300

    Добавлено условие в тесты для модификации групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,4 +1,6 @@
 from model.group import Group
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test1", header="test2", footer="test3"))
     app.group.edit_first(Group(name="тест", header="тестовый", footer="тестович"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,10 +1,16 @@
 from model.group import Group
 
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test1", header="test2", footer="test3"))
     app.group.modify_first_group(Group(name="New name"))
 
 def test_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test1", header="test2", footer="test3"))
     app.group.modify_first_group(Group(header="New header"))
 
 def test_modify_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test1", header="test2", footer="test3"))
     app.group.modify_first_group(Group(footer="New footer"))
