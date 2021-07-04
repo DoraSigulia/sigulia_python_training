f5df0b3c8cb5873941ccd4381f8267a5f717ae6d
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-07-04 23:25:38 +0300

    Для экономии 1 секунды теста

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,5 +2,5 @@ from model.group import Group
 
 def test_delete_first_group(app):
     if app.group.count() == 0:
-        app.group.create(Group(name="test1", header="test2", footer="test3"))
+        app.group.create(Group(name="test1"))
     app.group.delete_first_group()
