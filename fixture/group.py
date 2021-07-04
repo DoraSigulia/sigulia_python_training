8fc4d91da6f4c32ee8c9141dc490e0e51cb7a1ab
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-07-04 22:59:02 +0300

    Добавлено условие в тест test_delete_first_group

--- fixture/group.py
+++ fixture/group.py
@@ -63,3 +63,8 @@ class GroupHelper:
         #сохраняем форму
         wd.find_element_by_name("update").click()
         self.return_to_group()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_group_page()
+        return len(wd.find_elements_by_name("selected[]"))
