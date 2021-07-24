0afa932ae7b1e8978de70a8b8f8ff0fb9daddfa5
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-07-24 21:53:06 +0300

    Изменены проверки в удалении и добалении групп. Добавлена сортировка списков

--- model/group.py
+++ model/group.py
@@ -1,3 +1,5 @@
+from sys import maxsize
+
 class Group():
 
     def __init__(self, name=None, header=None, footer=None, id=None):
@@ -5,3 +7,15 @@ class Group():
         self.header = header
         self.footer = footer
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id), self.name == other.name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,12 +2,15 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups= app.group.get_group_list()
-    app.group.create(Group(name="test1", header="test2", footer="test3"))
+    group = Group(name="test1", header="test2", footer="test3")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+#def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()
+#   app.group.create(Group(name="", header="", footer=""))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) + 1 == len(new_groups)
