428d8882ba5af0ce223e188724362b22e6ddfe97
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-07-25 22:32:59 +0300

    Создано кеширование для групп

--- fixture/group.py
+++ fixture/group.py
@@ -22,6 +22,7 @@ class GroupHelper:
         self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
         self.return_to_group()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -43,6 +44,7 @@ class GroupHelper:
         # удалить группу
         wd.find_element_by_name("delete").click()
         self.return_to_group()
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -56,18 +58,22 @@ class GroupHelper:
         self.fill_group_form(new_group_date)
         wd.find_element_by_name("update").click()
         self.return_to_group()
+        self.group_cache = None
 
     def count(self):
         wd = self.app.wd
         self.open_group_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_group_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute('value')
-            groups.append(Group(name=text, id=id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_group_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute('value')
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
