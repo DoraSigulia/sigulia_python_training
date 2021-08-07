26bdff6214c04dc34b49e548e80a69c2ea3b8be5
DoraSigulia <78975023+DoraSigulia@users.noreply.github.com>
2021-08-08 00:38:07 +0300

    Добавлен регулярное выражение для замены символов

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,8 +1,12 @@
+import re
 
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_from_edit_page(0)
-    assert contact_from_home_page.homephone == contact_from_edit_page.homephone
-    assert contact_from_home_page.workphone == contact_from_edit_page.workphone
-    assert contact_from_home_page.mobile == contact_from_edit_page.mobile
-    assert contact_from_home_page.phone2 == contact_from_edit_page.phone2
+    assert contact_from_home_page.homephone == clear(contact_from_edit_page.homephone)
+    assert contact_from_home_page.workphone == clear(contact_from_edit_page.workphone)
+    assert contact_from_home_page.mobile == clear(contact_from_edit_page.mobile)
+    assert contact_from_home_page.phone2 == clear(contact_from_edit_page.phone2)
+
+def clear(s):
+    return re.sub("[-()]", "", s)
